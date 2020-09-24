c94d309cf11f9c349a0a0fcec3f6c487c5410d1a
alexangria1992 <alex040892@gmail.com>
2020-09-24 16:16:55 +1200

    Created intitial styling

--- index.php
+++ index.php
@@ -1,31 +1,122 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Title</title>
+    <title>Calendar App: Build</title>
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <style media="screen">
+        body {
+            font-family: 'Josefin Sans', sans-serif !important;
+            margin: 0;
+            background-color: #FEFDFD;
+            display: flex;
+            align-items: stretch;
 
+        }
+
+        .font {
+            font-family: 'Josefin Sans', sans-serif;
+
+        }
+
+        .default-cursor{
+            cursor: default ;
+        }
+
+        .center {
+            text-align: center;
+        }
+
+        .button{
+            border-radius: 500px !important;
+            margin: 0 auto !important;
+          
+        }
+
+        .color{
+            color: #FEFDFD;
+            background-color: #1B19CD;
+        }
+
+        .border-color{
+            border-color: #1B19CD;
+        }
+
+        .off-color{
+            color: #7C7EFB;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </style>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-      <div class="jumbotron">
+      <div id="current-day-info" class="color">
+          <h1 id="app-name-landscape" class="off-color center default-cursor">My Calendar</h1>
+          <div>
+              <h2 id="current-year" class="current-day-heading center default-cursor">2020</h2>
+          </div>
 
+          <div class="">
+              <h1 id="current-day" class="current-day-heading center default-cursor">Thursday</h1>
+              <h1 id="current-month" class="current-day-heading center default-cursor">September</h1>
+              <h1 id="current-date" class="current-day-heading center default-cursor">24</h1>
+
+              <button id="theme-landscape" type="button" name="button"  class="btn btn-success button font">Change Theme</button>
+
+          </div>
       </div>
-      
-    <!-- Optional JavaScript -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="js/jquery.min.js"></script>   
      <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-  <script>
-    $(document).ready(function(){
-          alert("hello");
-      })
-      </script>
+
   </script>
+  
   </body>
 
-  
 </html>
