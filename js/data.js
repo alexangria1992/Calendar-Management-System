fddcae0da696c6e46890e59b04b6da809c8957ae
alexangria1992 <alex040892@gmail.com>
2020-09-27 15:06:37 +1300

    Created function that dynamically shows the number of days in the month in relation to the current year

--- js/data.js
+++ js/data.js
@@ -14,8 +14,84 @@ var data = {
     }
 };
 
-var month_data = [
-    {
+// var month_data = [
+//     {
+//         month_index: 10,
+//         amount_of_days:30,
+//         starting_day: 4,
+//         year: 2019
+//     }, {
+//         month_index: 11,
+//         amount_of_days: 31,
+//         starting_day: 6,
+//         year: 2019
+//     }, {
+//         month_index: 0, 
+//         amount_of_days: 31,
+//         starting_day: 2,
+//         year: 2020
+//     }, {
+//         month_index: 1,
+//         amount_of_days: 29,
+//         starting_day: 5, 
+//         year: 2020
+//     }, {
+//         month_index: 2,
+//         amount_of_days: 31,
+//         starting_day: 5,
+//         year: 2020
+//     }, {
+//         month_index: 3,
+//         amount_of_days: 30,
+//         starting_day: 1,
+//         year: 2020
+//     }, {
+//         month_index: 4,
+//         amount_of_days: 31,
+//         starting_day: 3,
+//         year: 2020
+//     }, {
+//         month_index: 5,
+//         amount_of_days: 30,
+//         starting_day: 6,
+//         year: 2020
+//     }, {
+//         month_index: 6,
+//         amount_of_days: 31,
+//         starting_day: 1,
+//         year: 2020
+//     }, {
+//         month_index: 7,
+//         amount_of_days: 31,
+//         starting_day: 4,
+//         year: 2020
+//     }, {
+//         month_index: 8,
+//         amount_of_days: 30,
+//         starting_day: 9,
+//         year: 2020
+//     }, {
+//         month_index: 9,
+//         amount_of_days: 31,
+//         starting_day: 2,
+//         year: 2020
+//     }, {
+//         month_index: 10,
+//         amount_of_days: 30,
+//         starting_day: 5,
+//         year: 2020
+//     } , {
+//         month_index: 11,
+//         amount_of_days: 31,
+//         starting_day: 0,
+//         year: 2020
+//     } 
+    
+// ]
+
+function generateMonthData(maxYear)
+{
+    let data = [{
         month_index: 10,
         amount_of_days:30,
         starting_day: 4,
@@ -25,71 +101,26 @@ var month_data = [
         amount_of_days: 31,
         starting_day: 6,
         year: 2019
-    }, {
-        month_index: 0, 
-        amount_of_days: 31,
-        starting_day: 2,
-        year: 2020
-    }, {
-        month_index: 1,
-        amount_of_days: 29,
-        starting_day: 5, 
-        year: 2020
-    }, {
-        month_index: 2,
-        amount_of_days: 31,
-        starting_day: 5,
-        year: 2020
-    }, {
-        month_index: 3,
-        amount_of_days: 30,
-        starting_day: 1,
-        year: 2020
-    }, {
-        month_index: 4,
-        amount_of_days: 31,
-        starting_day: 3,
-        year: 2020
-    }, {
-        month_index: 5,
-        amount_of_days: 30,
-        starting_day: 6,
-        year: 2020
-    }, {
-        month_index: 6,
-        amount_of_days: 31,
-        starting_day: 1,
-        year: 2020
-    }, {
-        month_index: 7,
-        amount_of_days: 31,
-        starting_day: 4,
-        year: 2020
-    }, {
-        month_index: 8,
-        amount_of_days: 30,
-        starting_day: 9,
-        year: 2020
-    }, {
-        month_index: 9,
-        amount_of_days: 31,
-        starting_day: 2,
-        year: 2020
-    }, {
-        month_index: 10,
-        amount_of_days: 30,
-        starting_day: 5,
-        year: 2020
-    } , {
-        month_index: 11,
-        amount_of_days: 31,
-        starting_day: 0,
-        year: 2020
-    } 
-    
-]
+    }]
 
-function generateMonthData(2020)
-{
+    let prevMonth = data[data.length - 1];
+    let nextStartingDay = (prevMonth.amount_of_days + prevMonth.starting_day) % 7;
 
+    for(let year = 2019; year <= maxYear; year++)
+    {
+        for(let month = 0; month <= 11; month++){
+            let m = {
+                month_index: month,
+                amount_of_days: new Date(year, month + 1, 0).getDate(),
+                starting_day: nextStartingDay,
+                year: year
+            }
+
+            nextStartingDay = (m.amount_of_days + m.starting_day) % 7;
+            data.push(m);
+        }
+    }
+    return data;
 }
+
+var month_data = generateMonthData(2020);
